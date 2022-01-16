54d7f4b29bedc5057181f9c6855a88c68d271684
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2022-01-16 23:04:22 +0530

    Example of saving Student to table along with example of serializing and deserializing JSON

--- src/main/java/com/springrest/controller/StudentController.java
+++ src/main/java/com/springrest/controller/StudentController.java
@@ -3,15 +3,21 @@ package com.springrest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springrest.dao.StudentDAO;
 import com.springrest.model.Student;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+	private ObjectMapper objectMapper = new ObjectMapper();
 
 	@Autowired
 	private StudentDAO studentDAO;
@@ -21,4 +27,12 @@ public class StudentController {
 		return studentDAO.findById(id).get();
 	}
 
+	@PostMapping
+	public Student saveStudent(@RequestBody Student student) throws JsonProcessingException {
+		String studentStr = objectMapper.writeValueAsString(student); // Used to convert object to String
+		Student std = objectMapper.readValue(studentStr, Student.class); // Used to convert String to object
+		System.out.println("Value of Student in json: " + studentStr);
+		System.out.println("Value of Student bean in object form: " + std);
+		return studentDAO.save(student);
+	}
 }
